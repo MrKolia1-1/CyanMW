5931713eb7fcfd6a3f53305e403a2b95a265ad0f
XjCyan1de <xjcyan1de@gmail.com>
2017-01-20 06:44:00 +0300

    Добавление игроков в команды, определение победителя

--- src/me/xjcyan1de/cyanmw/Game.java
+++ src/me/xjcyan1de/cyanmw/Game.java
@@ -1,28 +1,142 @@
 package me.xjcyan1de.cyanmw;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+import org.bukkit.util.Vector;
 
 import static me.xjcyan1de.cyanmw.Main.prefix;
+import static me.xjcyan1de.cyanmw.Main.tmapi;
 import static org.bukkit.Bukkit.getServer;
 
-class Game {
+class Game implements Listener{
+    static boolean GameRunning = true;
+    private static World world = getServer().getWorld("world");
+    static int GreenSize = 0;
+    static int RedSize = 0;
+    static Location RedSpawn = new Location(world, 71.5, 75, -64.5, 0, 0);
+    static Location GreenSpawn = new Location(world, 71.5, 75, 65.5, 180, 0);
 
-    static boolean GameRuning = true;
+    static void AddPlayer() {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            int x = p.getLocation().getBlockX();
+            int y = p.getLocation().getBlockY();
+            int z = p.getLocation().getBlockZ();
+            if (x == -14 && y == 72 && z == 2) {
+                JoinGame(p);
+            }
+            if (x == -14 && y == 72 && z == 1) {
+                JoinGame(p);
+            }
+            if (x == -14 && y == 72 && z == 0) {
+                JoinGame(p);
+            }
+            if (x == -14 && y == 72 && z == -1) {
+                JoinGame(p);
+            }
+            if (x == -14 && y == 72 && z == -2) {
+                JoinGame(p);
+            }
+        }
+    }
+
 
     static void GameEnd() {
-        World world = getServer().getWorld("world");
-        if (!world.getBlockAt(70, 70, -72).getType().equals(Material.PORTAL) || !world.getBlockAt(72, 70, -72).getType().equals(Material.PORTAL)) {
-            Bukkit.broadcastMessage(prefix+"У красных нет портала");
-            GameRuning = false;
+        //зелёный портал
+        if (!world.getBlockAt(70, 70, 72).getType().equals(Material.PORTAL) || !world.getBlockAt(72, 70, 72).getType().equals(Material.PORTAL)) {
+            Bukkit.broadcastMessage(prefix + "У зелёных нет портала");
+            GameRunning = false;
             Main.PortalCheck.cancelAllTasks();
+            WonGame("RED");
+        }
+        //красный портал
+        if (!world.getBlockAt(70, 70, -72).getType().equals(Material.PORTAL) || !world.getBlockAt(72, 70, -72).getType().equals(Material.PORTAL)) {
+            GameRunning = false;
+            Main.PortalCheck.cancelAllTasks();
+            WonGame("GREEN");
+        }
+    }
+
+    static void WonGame(String team) {
+        if (team.equals("GREEN")) {
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                tmapi.sendTitles(p, "§aЗелёная команда", "§aпобедила!");
+                p.setGameMode(GameMode.SPECTATOR);
+                //world.getBlockAt(71, 80, 66).setType(Material.STRUCTURE_BLOCK);
+            }
+        }
+        if (team.equals("RED")) {
+            for (Player p : Bukkit.getOnlinePlayers()) {
+                tmapi.sendTitles(p, "§cКрасная команда", "§cпобедила!");
+                p.setGameMode(GameMode.SPECTATOR);
+            }
+        }
+    }
+
+
+    static void JoinGame(Player p) {
+        p.sendMessage("ты заходишь");
+        Scoreboard scoreboard = p.getScoreboard();
+        Team Red = null;
+        Team Green = null;
+        try {
+            Red = scoreboard.registerNewTeam("Красные");
+            Green = scoreboard.registerNewTeam("Зелёные");
+            Red.setPrefix("§c");
+            Green.setPrefix("§a");
+        } catch (IllegalArgumentException ignored){}
+
+        if (RedSize >= GreenSize) {
+            scoreboard.getTeam("Зелёные").addEntry(p.getName());
+            GreenSize = GreenSize+1;
+
+            p.setBedSpawnLocation(GreenSpawn);
+            p.teleport(GreenSpawn);
+
+            ItemStack bow = new ItemStack(Material.BOW);
+            bow.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 5);
+            p.getInventory().addItem(bow);
+
+        } else {
+            scoreboard.getTeam("Красные").addEntry(p.getName());
+            RedSize = RedSize+1;
+
+            p.teleport(RedSpawn);
+
+            ItemStack bow = new ItemStack(Material.BOW);
+            bow.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 5);
+            p.getInventory().addItem(bow);
         }
 
-        if (!world.getBlockAt(70, 70, 72).getType().equals(Material.PORTAL) || !world.getBlockAt(72, 70, 72).getType().equals(Material.PORTAL)) {
-            Bukkit.broadcastMessage(prefix+"У зелёных нет портала");
-            GameRuning = false;
-            Main.PortalCheck.cancelAllTasks();
+        p.sendMessage("Красные "+RedSize);
+        p.sendMessage("Зелёные "+GreenSize);
+    }
+
+    @EventHandler
+    public void DeathScreen(PlayerDeathEvent e) {
+        Player p = e.getEntity();
+        p.setVelocity(new Vector(0, 0, 0));
+        p.spigot().respawn();
+    }
+
+    @EventHandler
+    public void Respawn(PlayerRespawnEvent e) {
+        Player p = e.getPlayer();
+        String pname = p.getName();
+        if (p.getScoreboard().getEntryTeam(pname).getName().equals("Красные")) {
+            e.setRespawnLocation(RedSpawn);
+            p.setVelocity(new Vector(0, -10, 0));
+        }
+        if (p.getScoreboard().getEntryTeam(pname).getName().equals("Зелёные")) {
+            e.setRespawnLocation(GreenSpawn);
+            p.setVelocity(new Vector(0, -10, 0));
         }
     }
 }
--- src/me/xjcyan1de/cyanmw/Main.java
+++ src/me/xjcyan1de/cyanmw/Main.java
@@ -1,32 +1,43 @@
 package me.xjcyan1de.cyanmw;
 
+import io.puharesource.mc.titlemanager.api.v2.TitleManagerAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
-import static me.xjcyan1de.cyanmw.Game.GameRuning;
+import static me.xjcyan1de.cyanmw.Game.GameRunning;
 
 public class Main extends JavaPlugin {
 
-    static String prefix = "[§cMissile§aWars§r] ";
-    static CommandSender console = Bukkit.getConsoleSender();
-    Main plugin = this;
+    static String prefix = "[CyanMW] ";
+    private static CommandSender console = Bukkit.getConsoleSender();
     static BukkitScheduler PortalCheck = Bukkit.getServer().getScheduler();
+    private static BukkitScheduler JoinGame = Bukkit.getServer().getScheduler();
+    public static TitleManagerAPI tmapi;
 
     public void onEnable() {
         console.sendMessage(prefix + "§rВключен!");
 
-        checkPortal();
+        tmapi = (TitleManagerAPI) Bukkit.getServer().getPluginManager().getPlugin("TitleManager");
+        Bukkit.getPluginManager().registerEvents(new Game(), this);
+        schedulerCheckPortal();
+        schedulerAddPlayer();
     }
 
     public void onDisable() {
         console.sendMessage(prefix + "§rВыключен!");
     }
 
-    public void checkPortal() {
-        if (GameRuning) {
+    private void schedulerCheckPortal() {
+        if (GameRunning) {
             PortalCheck.scheduleSyncRepeatingTask(this, Game::GameEnd, 0L, 5L);
+        } else Main.PortalCheck.cancelAllTasks();
+    }
+
+    private void schedulerAddPlayer() {
+        if (GameRunning) {
+            JoinGame.scheduleSyncRepeatingTask(this, Game::AddPlayer, 0L, 5L);
         }
     }
 }
