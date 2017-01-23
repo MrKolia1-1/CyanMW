3fe2f2a4bb91da49322d371d02b0aee3a8a73069
XjCyan1de <xjcyan1de@gmail.com>
2017-01-23 08:59:02 +0300

    Добавил описание к ракетам

--- src/me/xjcyan1de/cyanmw/GiveItem.java
+++ src/me/xjcyan1de/cyanmw/GiveItem.java
@@ -5,11 +5,20 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
+
 public class GiveItem {
     public static void GreenGuardian(Player p) {
         ItemStack item = SpawnEgg.get(EntityType.GUARDIAN, 1);
         ItemMeta meta = item.getItemMeta();
+        ArrayList<String> lore = new ArrayList<String>();
         meta.setDisplayName("§aЗелёный страж");
+        lore.add(" ");
+        lore.add("§7Защищает от стрел");
+        lore.add(" ");
+        lore.add("§71.7 Блоков/сек");
+        lore.add("§74 TNT");
+        meta.setLore(lore);
         item.setItemMeta(meta);
         if (!p.getInventory().containsAtLeast(item, 1)) {
             p.getInventory().addItem(item);
@@ -18,7 +27,14 @@ public class GiveItem {
     public static void RedGuardian(Player p) {
         ItemStack item = SpawnEgg.get(EntityType.GUARDIAN, 1);
         ItemMeta meta = item.getItemMeta();
+        ArrayList<String> lore = new ArrayList<String>();
         meta.setDisplayName("§cКрасный страж");
+        lore.add(" ");
+        lore.add("§7Защищает от стрел");
+        lore.add(" ");
+        lore.add("§71.7 Блоков/сек");
+        lore.add("§74 TNT");
+        meta.setLore(lore);
         item.setItemMeta(meta);
         if (!p.getInventory().containsAtLeast(item, 1)) {
             p.getInventory().addItem(item);
@@ -27,7 +43,14 @@ public class GiveItem {
     public static void GreenLightning(Player p) {
         ItemStack item = SpawnEgg.get(EntityType.OCELOT, 1);
         ItemMeta meta = item.getItemMeta();
+        ArrayList<String> lore = new ArrayList<String>();
         meta.setDisplayName("§aЗелёная молния");
+        lore.add(" ");
+        lore.add("§7Высокоскростная ракета");
+        lore.add(" ");
+        lore.add("§73.3 Блоков/сек");
+        lore.add("§712 TNT");
+        meta.setLore(lore);
         item.setItemMeta(meta);
         if (!p.getInventory().containsAtLeast(item, 1)) {
             p.getInventory().addItem(item);
@@ -36,7 +59,14 @@ public class GiveItem {
     public static void RedLightning(Player p) {
         ItemStack item = SpawnEgg.get(EntityType.OCELOT, 1);
         ItemMeta meta = item.getItemMeta();
+        ArrayList<String> lore = new ArrayList<String>();
         meta.setDisplayName("§cКрасная молния");
+        lore.add(" ");
+        lore.add("§7Высокоскростная ракета");
+        lore.add(" ");
+        lore.add("§73.3 Блоков/сек");
+        lore.add("§712 TNT");
+        meta.setLore(lore);
         item.setItemMeta(meta);
         if (!p.getInventory().containsAtLeast(item, 1)) {
             p.getInventory().addItem(item);
@@ -45,7 +75,14 @@ public class GiveItem {
     public static void GreenJuggernaut(Player p) {
         ItemStack item = SpawnEgg.get(EntityType.GHAST, 1);
         ItemMeta meta = item.getItemMeta();
+        ArrayList<String> lore = new ArrayList<String>();
         meta.setDisplayName("§aЗелёный Джаггернаут");
+        lore.add(" ");
+        lore.add("§7Огромный урон");
+        lore.add(" ");
+        lore.add("§71.7 Блоков/сек");
+        lore.add("§722 TNT");
+        meta.setLore(lore);
         item.setItemMeta(meta);
         if (!p.getInventory().containsAtLeast(item, 1)) {
             p.getInventory().addItem(item);
@@ -54,7 +91,14 @@ public class GiveItem {
     public static void RedJuggernaut(Player p) {
         ItemStack item = SpawnEgg.get(EntityType.GHAST, 1);
         ItemMeta meta = item.getItemMeta();
+        ArrayList<String> lore = new ArrayList<String>();
         meta.setDisplayName("§cКрасный Джаггернаут");
+        lore.add(" ");
+        lore.add("§7Огромный урон");
+        lore.add(" ");
+        lore.add("§71.7 Блоков/сек");
+        lore.add("§722 TNT");
+        meta.setLore(lore);
         item.setItemMeta(meta);
         if (!p.getInventory().containsAtLeast(item, 1)) {
             p.getInventory().addItem(item);
@@ -63,7 +107,14 @@ public class GiveItem {
     public static void GreenShieldbuster(Player p) {
         ItemStack item = SpawnEgg.get(EntityType.WITCH, 1);
         ItemMeta meta = item.getItemMeta();
+        ArrayList<String> lore = new ArrayList<String>();
         meta.setDisplayName("§aЗелёный противобарьер");
+        lore.add(" ");
+        lore.add("§7Пробивает барьер");
+        lore.add(" ");
+        lore.add("§71.7 Блоков/сек");
+        lore.add("§717 TNT");
+        meta.setLore(lore);
         item.setItemMeta(meta);
         if (!p.getInventory().containsAtLeast(item, 1)) {
             p.getInventory().addItem(item);
@@ -72,7 +123,14 @@ public class GiveItem {
     public static void RedShieldbuster(Player p) {
         ItemStack item = SpawnEgg.get(EntityType.WITCH, 1);
         ItemMeta meta = item.getItemMeta();
+        ArrayList<String> lore = new ArrayList<String>();
         meta.setDisplayName("§cКрасный противобарьер");
+        lore.add(" ");
+        lore.add("§7Пробивает барьер");
+        lore.add(" ");
+        lore.add("§71.7 Блоков/сек");
+        lore.add("§717 TNT");
+        meta.setLore(lore);
         item.setItemMeta(meta);
         if (!p.getInventory().containsAtLeast(item, 1)) {
             p.getInventory().addItem(item);
@@ -81,7 +139,14 @@ public class GiveItem {
     public static void GreenTomahawk(Player p) {
         ItemStack item = SpawnEgg.get(EntityType.CREEPER, 1);
         ItemMeta meta = item.getItemMeta();
+        ArrayList<String> lore = new ArrayList<String>();
         meta.setDisplayName("§aЗелёный томагавк");
+        lore.add(" ");
+        lore.add("§7Фронтальный взрыв");
+        lore.add(" ");
+        lore.add("§71.7 Блоков/сек");
+        lore.add("§715 TNT");
+        meta.setLore(lore);
         item.setItemMeta(meta);
         if (!p.getInventory().containsAtLeast(item, 1)) {
             p.getInventory().addItem(item);
@@ -90,7 +155,14 @@ public class GiveItem {
     public static void RedTomahawk(Player p) {
         ItemStack item = SpawnEgg.get(EntityType.CREEPER, 1);
         ItemMeta meta = item.getItemMeta();
+        ArrayList<String> lore = new ArrayList<String>();
         meta.setDisplayName("§cКрасный томагавк");
+        lore.add(" ");
+        lore.add("§7Фронтальный взрыв");
+        lore.add(" ");
+        lore.add("§71.7 Блоков/сек");
+        lore.add("§715 TNT");
+        meta.setLore(lore);
         item.setItemMeta(meta);
         if (!p.getInventory().containsAtLeast(item, 1)) {
             p.getInventory().addItem(item);
